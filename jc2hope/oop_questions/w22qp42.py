cfb5e8f9a00beae4b3652bca627b24c962c1aa90
cherylaurelia <cherylaurelia311@gmail.com>
2025-11-25 23:12:25 +0700

    h

--- jc2hope/oop_questions/w22qp42.py
+++ jc2hope/oop_questions/w22qp42.py
@@ -24,15 +24,57 @@ class Character:
 Characters = []
 
 try:
-    file = open("Characters.txt", 'r')
+    file = open("jc2hope/Characters.txt", 'r')
     line = file.readline().strip()
-    while line != "":
+    while line != "" and (len(Characters) < 10):
         name = line
-        xcoord = file.readline().strip()
-        ycoord = file.readline.strip()
+        xcoord = int(file.readline().strip())
+        ycoord = int(file.readline().strip())
         Characters.append(Character(name, xcoord, ycoord))
         line = file.readline().strip()
 
 except FileNotFoundError as e:
     print(f"File not found! {e}")
+
+#e
+    
+nameinput = str(input("Enter a name: "))
+
+while True:
+    found = False
+    for character in Characters:
+        if character.GetName() == nameinput:
+            found = True
+            break
+    if found:
+        break
+    nameinput = input("Enter a name: ")
+
+
+for j in range(len(Characters)):
+    if Characters[j].GetName() == nameinput:
+        idx = j
         
+#f
+     
+print(Characters[idx].GetX(), Characters[idx].GetY())
+
+letterinput = str(input("Enter a direction (A, W, S, D): "))
+
+valid = ["a", "w", "s", "d", "A", "W", "S", "D"]
+
+while letterinput not in (valid):
+    letterinput = str(input("Enter a direction (A, W, S, D): "))
+
+lowerletterinput = letterinput.lower()
+
+if lowerletterinput == "a":
+    Characters[idx].ChangePosition(-1,0)
+elif lowerletterinput == "w":
+    Characters[idx].ChangePosition(0,1)
+elif lowerletterinput == "s":
+    Characters[idx].ChangePosition(0,-1)
+elif lowerletterinput == "d":
+    Characters[idx].ChangePosition(1,0)
+
+print(Characters[idx].GetX(), Characters[idx].GetY())
