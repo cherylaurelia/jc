4d870aa411d8df6f08721e7c06f145d391c1ad46
cheryl <135518448+cherylaurelia@users.noreply.github.com>
2026-02-24 22:01:02 +0700

    Update recursion1.py

--- jc2hope/recursion1.py
+++ jc2hope/recursion1.py
@@ -36,11 +36,11 @@ def countdown(n):
 
 #a)
 
-def reverseString(str):
-    if len(str) == 1:
-        return str
-    else:
-        return 0 #uh
+def reverseString(s):
+    if s == "":
+        return ""
+
+    return reverseString(s[1:]) + s[0]
 
 
     
@@ -59,6 +59,57 @@ def power(base, exp):
 #a)
 
 def countDigits(n):
+    if n < 10:
+        return 1
+    return 1 + countDigits(n // 10)
 
+#Q7
+
+#a)
+
+def fib(n):
+    if n == 0:
+        return 0
+
+    if n == 1:
+        return 1
+
+    return fib(n-1) + fib(n-2)
+
+
+#Q8
+
+#a)
+
+def sumOfArray(arr):
+    if len(arr) == 0:
+        return 0
+
+    return arr[0] + sumOfArray(arr[1:])
+
+#Q9
+
+#a)
+
+def binarysearch(arr, se, lb, ub):
+    if low > high:
+        return -1
+
+    mid = (lb + ub) // 2
+
+    if arr[mid] == se:
+        return mid
+    elif se < arr[mid]:
+        return binarySearch(arr, se, lb, mid - 1)
+    else:
+        return binarySearch(arr, se, mid + 1, ub)
+
+#Q10
+
+#a)
+
+def hanoi(n, source, auxiliary, target):
+    return 1
         
         
+
